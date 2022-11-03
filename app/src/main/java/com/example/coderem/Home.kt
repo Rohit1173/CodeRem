e4f39e817a718cb35064348ca162ac89e7fd516b
Rohit1173 <lcs2020048@iiitl.ac.in>
2022-11-03 19:05:16 +0530

    changes

--- app/src/main/java/com/example/coderem/Home.kt
+++ app/src/main/java/com/example/coderem/Home.kt
@@ -13,36 +13,37 @@ class Home : Fragment() {
     lateinit var vm: ContestViewModel
 
 
-   lateinit var binding: FragmentHomeBinding
+    lateinit var binding: FragmentHomeBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        binding=FragmentHomeBinding.inflate(inflater,container,false)
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
         binding.shimmerLayout.startShimmer()
         vm = ViewModelProvider.AndroidViewModelFactory(requireActivity().application)
             .create(ContestViewModel::class.java)
 
 
-        vm.myevent.observe(viewLifecycleOwner){
-            if(it.size>0){
+        vm.myevent.observe(viewLifecycleOwner) {
+            if (it.size > 0) {
                 binding.shimmerLayout.stopShimmer()
-                binding.shimmerLayout.visibility=View.GONE
-                binding.recycler.visibility=View.VISIBLE
+                binding.shimmerLayout.visibility = View.GONE
+                binding.recycler.visibility = View.VISIBLE
 
             }
-            val ml:MutableList<CodeData> = mutableListOf()
-            for(i in it){
-                if(i.site!="HackerRank"&&i.site!="HackerEarth"){
+            val ml: MutableList<CodeData> = mutableListOf()
+            for (i in it) {
+                if (i.site != "HackerRank" && i.site != "HackerEarth") {
                     ml.add(i);
                 }
             }
-            binding.recycler.adapter=DataAdapter(ml)
+            binding.recycler.adapter = DataAdapter(ml)
             binding.recycler.setHasFixedSize(true)
         }
-        val list:MutableList<String> = mutableListOf("CODEFORCES","CODECHEF","LEETCODE","ATCODER","KICKSTART")
-        binding.names.adapter=FilterAdapter(list)
+        val list: MutableList<String> =
+            mutableListOf("CODEFORCES", "CODECHEF", "LEETCODE", "ATCODER", "KICKSTART")
+        binding.names.adapter = FilterAdapter(list)
         binding.names.setHasFixedSize(true)
 
 
